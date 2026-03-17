f3e9713840db5497f368734485986f8349e1364c
dgusgus <dgusgus1@gmail.com>
2026-03-17 10:21:45 -0400

    cambios para el primer deploy2

--- vite.config.js
+++ vite.config.js
@@ -46,7 +46,7 @@ export default defineConfig({
         background_color: '#0f1117',
         display: 'standalone',        // ← Sin barra del navegador
         orientation: 'portrait',
-        start_url: '/DashboardDbSQLite/operadores',
+        start_url: '/DashboardDbSQLite/',
         scope: '/DashboardDbSQLite/',
         icons: [
           { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
